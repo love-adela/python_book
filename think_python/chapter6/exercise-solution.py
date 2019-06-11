6d85d66d90918f540ae5143a63cccb02f9c04a66
love-adelar <love.adelar@gmail.com>
2019-06-11 21:45:18 +0900

    Chapter6 Exercise

--- think_python/chapter6/exercise-solution.py
+++ think_python/chapter6/exercise-solution.py
@@ -21,12 +21,13 @@ print(c(x, y + 3, x + y))
 
 # Exercise 6-2
 
-def ack(x, y):
-    if x == 0:
-        return y + 1
-    if y == 0 :
-        return ack(x-1, 1)
-    return ack(x-1, ack(x, y-1))
+def ack(m, n):
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0 :
+        return ack(m-1, 1)
+    else:
+        return ack(m-1, ack(m, n-1))
 
 print(ack(3, 4)) # 125
 
@@ -73,4 +74,3 @@ def euclidean(a,b):
         return euclidean(b, a%b)
 
 print(euclidean(105, 252))
-
--- think_python/chapter6/palindrome.py
+++ think_python/chapter6/palindrome.py
@@ -7,19 +7,15 @@ def last(word):
 def middle(word):
     return word[1:-1]
 
-print(first('zerodarkthirty'))
-print(last('zerodarkthirty'))
-print(middle('zerodarkthirty'))
-
-# Error
-# print(first(''))
-# print(last(''))
-# print(middle(''))
-
 def is_palindrome(word):
-    if word == word[::-1]:
-            return True
+    if len(word) < 2:
+        return True
     else:
-            return False
-		
-print(is_palindrome("adeleda"))
+        return first(word) == last(word) and is_palindrome(middle(word))
+    
+print(is_palindrome('')) # True
+print(is_palindrome('a')) # True
+print(is_palindrome('aa')) # True
+print(is_palindrome('ab')) # False
+print(is_palindrome("aba")) # True
+
